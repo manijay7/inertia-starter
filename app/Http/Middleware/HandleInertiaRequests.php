5bff2b41eac344e883b9e57d0920a5d5a89d016e
Manuel Adjei <majbrnn@gmail.com>
2023-08-18 15:51:13 +0000

    add key for currentRouteName

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Inertia\Middleware;
 use Tightenco\Ziggy\Ziggy;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -39,11 +40,13 @@ class HandleInertiaRequests extends Middleware
     {
         return array_merge(parent::share($request), [
             'ziggy' => function () use ($request) {
-                return array_merge((new Ziggy)->toArray(), [
+                return array_merge((new Ziggy())->toArray(), [
                     'location' => $request->url(),
                 ]);
             },
             'app.name' => config('app.name'),
+            'currentRouteName' => Route::currentRouteName()
+
             // 'auth.user' => fn() => $request->user()
             // ? new UserSharedResource($request->user()->with('church')->first())
             // : null,
